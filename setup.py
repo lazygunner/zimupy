9a222f8bb9e45d0d0adc9b2e08e72b19a5118029
gunner <gymgunner@gmail.com>
2016-12-24 21:05:09 +0800

    Update pypi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='zimupy',
-    version='0.0.2',
+    version='0.0.4',
     author='lazygunner',
     author_email='gymgunner@gmail.com',
     url='https://github.com/lazygunner/zimupy',
